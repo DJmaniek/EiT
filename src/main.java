efe6930ef8d06dad03f33db47bf8716b46a02b8b
DJmaniek <crouchergames@gmail.com>
2021-03-11 15:56:32 +0100

    MS

--- src/main.java
+++ src/main.java
@@ -1,5 +1,6 @@
 public class main {
     public static void main(String[] args){
-        System.out.println("JDK wersja 13.0.1 JRE wersja 1.7");
+
+        System.out.println("Mariusz Skwiercz");
     }
 }
