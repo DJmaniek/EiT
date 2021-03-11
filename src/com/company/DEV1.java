033b308566553c9fdfa10e3b0f9ad014a950641f
DJmaniek <crouchergames@gmail.com>
2021-03-11 16:23:09 +0100

    MS

---
+++ src/com/company/DEV1.java
@@ -0,0 +1,5 @@
+package com.company;
+
+public class DEV1 {
+    Autor Mariusz Skwiercz
+}
--- src/main.java
+++ src/main.java
@@ -1,6 +1,6 @@
 public class main {
     public static void main(String[] args){
 
-        System.out.println("Mariusz Skwiercz");
+        System.out.println("JDK wersja 13.0.1 JRE wersja 1.7");
     }
 }
