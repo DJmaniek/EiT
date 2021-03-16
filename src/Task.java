7e8927b7be36c2278f03a05fb75e9486fd98ba98
DJmaniek <crouchergames@gmail.com>
2021-03-16 11:21:28 +0100

    MS

---
+++ src/Task.java
@@ -0,0 +1,24 @@
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.lang.System;
+
+public class Task {
+    Date DataZ = new Date(System.currentTimeMillis());
+    SimpleDateFormat Format = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        private String taskName;
+        private String taskDescription;
+        private Category taskCategory;
+        private String taskDateTimeStop;
+        private String taskDateTimeStart = DataZ.toString();
+
+    public Task (String taskName, String taskDescription, Category taskCategory, String taskDateTimeStop) {
+        this.taskName = taskName;
+        this.taskDescription = taskDescription;
+        this.taskCategory = taskCategory;
+        this.taskDateTimeStop = taskDateTimeStop;
+    }
+    public String toString() {
+        return "Name: " + taskName + ", Description: " + taskDescription + ", Category: " + taskCategory.name() +
+                ", Time start: " + taskDateTimeStart + ", Time stop: " + taskDateTimeStop;
+    }
+}
