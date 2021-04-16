f38369fbd71b20778704cb10eb35d75d8bbc9aff
ArturKnopek <80393611+ArturKnopek@users.noreply.github.com>
2021-04-16 09:56:17 +0200

    Update TaskManager.java

--- src/com/company/TaskManager.java
+++ src/com/company/TaskManager.java
@@ -9,15 +9,13 @@ public interface TaskManager {
     Task[] getAllTasks();
     Task getTaskByName(String taskName);
     public static final Task [] tasks = { //podalem konkretne przechowywane wartosci, aczkolwiek mozna dodac tutaj pusta tablice za pomoca "public static final Task [] tasks = new Task[10];"
-            new Task("Bieganie", "Przebiec 10km", taskCategory.NEW),
-            new Task("Czytanie", "Przeczytac ksiazke", taskCategory.DONE),
-            new Task("Pływanie", "isc poplywac",taskCategory.NEW),
-            new Task("Cwiczenie", "Pojsc na silownie", taskCategory.IN_PROGRESS),
-            new Task("Szachy", "Zagrac w szachy",taskCategory.DONE),
-            new Task("Gotowanie", "Ugotowac obiad", taskCategory.NEW),
-            new Task("Sprzatanie", "Posprzatac dom", taskCategory.IN_PROGRESS),
-            new Task("Spotkanie", "Spotkac sie ze znajomymi", taskCategory.DONE),
-            new Task("Nauka", "Nauczyc sie programowania", taskCategory.NEW),
+            new Task("Bieganie", "Przebiec 3k w 30min", taskCategory.NEW),
+            new Task("Czytanie", "Przeczytac artukół w internecie", taskCategory.DONE),
+            new Task("Cwiczenie", "Poćwiczyć programowanie", taskCategory.IN_PROGRESS),
+            new Task("Szachy", "Zagrac w szachy z kolegami",taskCategory.DONE),
+            new Task("Gotowanie", "Ugotowac posiłek na pare dni", taskCategory.NEW),
+            new Task("Sprzatanie", "Posprzatac samochod", taskCategory.IN_PROGRESS),
+            new Task("Nauka", "Nauczyc sie czegoś porzytecznego", taskCategory.NEW),
             new Task("Programowanie", "Zrobic zadanie na programowanie", taskCategory.IN_PROGRESS),
 
     };
